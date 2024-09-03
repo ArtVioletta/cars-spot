ae3510072aa953512834131867a79b6c3bd6df7a
Viola <violetta.artiukhova@skey.team>
2024-09-03 09:41:38 +0200

    Added new design for header and hero section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Header from "../components/Header/Header";
+import HeroSection from "../components/HeroSection/HeroSection";
 
 
 const HomePage = () => {
     return(
-        <div>
-
-        </div>
+       <>
+       <Header/>
+       <HeroSection/>
+       </>
     )
 }
 
