64f666f1b9cb9f769f70178007e2e07f3f8e5701
Viola <violetta.artiukhova@skey.team>
2024-09-04 18:53:08 +0200

    changed css header and footer

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -3,10 +3,10 @@ import { carsFooter } from '../../shared/data/carsFooter';
 
 const Footer = () => {
   return (
-    <footer className="bg-custom-gray text-white mt-6 sm:mt-80 md:mt-6  ">
+    <footer className="bg-custom-gray text-white mt-6 sm:mt-20 md:mt-6  ">
       <div className="grid grid-cols-1 md:grid-cols-2 mb-8 pt-20 container flex flex-col gap-1  mx-auto px-4  ">
         {carsFooter.map((item) => (
-          <div key={item.id} className="">
+          <div key={item.id} className="mb-5">
             <h3 className="font-bold text-2xl leading-9 font-bebas-neue mb-2">
               {item.header}
             </h3>
